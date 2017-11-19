224c813f265b7cf1dea8fcbdc2e0ba7db0db9789
Manatsawin Hanmongkolchai <manatsawin+git@gmail.com>
2017-11-19 12:07:39 +0700

    v1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='django-zipkin-trace',
-    version='1.0.0',
+    version='1.0.1',
     url='https://github.com/whs/django-zipkin-trace',
     author='Manatsawin Hanmongkolchai',
     author_email='manatsawin+pypi@gmail.com',
